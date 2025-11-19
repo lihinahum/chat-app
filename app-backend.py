1da9ac0d615ac6abd5a1b4c7c90c9a4985ae4bec
Salih <salihdaher9@gmail.com>
2025-11-19 12:31:21 +0200

    added home page route

--- app-backend.py
+++ app-backend.py
@@ -3,5 +3,10 @@ from flask import Flask,redirect, url_for , render_template,request
 app = Flask(__name__)
 
 
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
