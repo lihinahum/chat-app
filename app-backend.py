99d5175db737b83e3f79712fdb52a9948f5b11d5
Salih <salihdaher9@gmail.com>
2025-11-19 13:37:32 +0200

    implemented get chat route

--- app-backend.py
+++ app-backend.py
@@ -1,6 +1,10 @@
 from flask import Flask,redirect, url_for , render_template,request
+import os
 
 app = Flask(__name__)
+
+CHAT_DIR = "chats"   # env variable for chat directory
+
 
 @app.route("/<room>")
 def room(room):
@@ -17,7 +21,14 @@ def chat(room):
         
         return 
     else:
-        return 
+        file_path = os.path.join(CHAT_DIR, f"{room}.txt")
+
+        if not os.path.exists(file_path):
+            return "file donest exist", 404
+    
+        with open(file_path, "r", encoding="utf-8") as f:
+            print
+            return f.read()  
 
 
 
