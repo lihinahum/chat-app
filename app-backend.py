6d88c0971bfabcdc291ed2e590a79df85c6d3929
Lihi Nahum <lihina4@gmail.com>
2025-11-19 12:36:09 +0200

    add room route

--- app-backend.py
+++ app-backend.py
@@ -2,6 +2,10 @@ from flask import Flask,redirect, url_for , render_template,request
 
 app = Flask(__name__)
 
+@app.route("/<room>")
+def room(room):
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
